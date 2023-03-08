7320fe887f003a2d6cd5595d5e75f172574d6894
Adel Yudina <adelyudina@gmail.com>
2023-03-08 14:27:28 +0300

    Congrats Bill

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -7,8 +7,9 @@ import prompt
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()
+    name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
+    success = 0
     for tries in range(0, 3):
         number = randint(0, 100)
         print(f"Question: {number}")
@@ -16,17 +17,21 @@ def main():
         if number % 2 == 0:
             if answer == 'yes':
                 print('Correct!')
+                success += 1 
             else:
-                print('Incorrect!')
+                print(f'"no" is wrong answer ;(. Correct answer was "yes".\nLet\'s try again, {name}!')
+                break
         else:
             if number % 2 != 0:
                 if answer == 'no':
                     print('Correct!')
+                    success += 1
                 else:
-                    print('Incorrect!')
+                    print(f'"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again, {name}!')
+                    break
     
-
-#print('Congratulations, Bill!') 
+    if success == 3:  
+        print(f'Congratulations, {name}!') 
            
 
 if __name__ == '__main__':
