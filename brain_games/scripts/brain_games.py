53261496ed76a0fe8a5e71da45bd6aeb594968d0
Adel Yudina <adelyudina@gmail.com>
2023-03-08 13:50:07 +0300

    add for loop

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,32 @@
 
 from brain_games.cli import welcome_user
 from random import randint
+import prompt
 
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
-    print(randint(0, 100))
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for tries in range(0, 3):
+        number = randint(0, 100)
+        print(f"Question: {number}")
+        answer = prompt.string('Your answer: ')
+        if number % 2 == 0:
+            if answer == 'yes':
+                print('Correct!')
+            else:
+                print('Incorrect!')
+        else:
+            if number % 2 != 0:
+                if answer == 'no':
+                    print('Correct!')
+                else:
+                    print('Incorrect!')
+    
+
+#print('Congratulations, Bill!') 
+           
 
 if __name__ == '__main__':
     main()
