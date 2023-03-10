83f484f7e324e7a2f71af2c68a97b1f898090cb9
Adel Yudina <adelyudina@gmail.com>
2023-03-10 21:09:13 +0300

    updated calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,6 +2,9 @@
 
 from random import randint
 from random import choice 
+from brain_games.cli import welcome_user
+import prompt
+
 
 def calc():
     f_num = randint(0, 100)
@@ -21,6 +24,28 @@ def calc():
         result = f_num * s_num 
 
     return expression, result 
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print('What is the result of the expression?')
+    success = 0
+    for tries in range(0, 3):
+        expression = def calc()
+        result = def calc()
+        print(f"Question: {expression}")
+        answer = prompt.string('Your answer: ')
+        if result == answer:
+             print('Correct!')
+                 success += 1
+        else:
+             print(f'"{answer}" is wrong answer ;(. Correct answer was "{result}".\nLet\'s try again, {name}!')
+                 break
+
+    if success == 3:
+        print(f'Congratulations, {name}!')
+
      
 if __name__ == '__main__':
     main()
