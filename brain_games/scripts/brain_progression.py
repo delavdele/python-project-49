5ac15c97380c044e5a346a87698795467bfa6d76
Adel Yudina <adelyudina@gmail.com>
2023-03-15 20:50:17 +0300

    finished progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -7,8 +7,8 @@ import prompt
 from brain_games.cli import welcome_user
 
 def progression():
-    f_num = randint(0, 10)
-    s_num = randint(0, 10)
+    f_num = randint(1, 10)
+    s_num = randint(1, 10)
     random_index = randrange(1, 11)
     none_object = 0
     for i in range(1, 11):
@@ -17,24 +17,24 @@ def progression():
             print('..', end=' ')
             none_object = num1 
         else:
-            print(num1, sep=' ', end=' ')        
-    print('\n')
-    return num1
+            print(num1, end=' ')
+    print()        
+    return none_object 
 
 def main():
     print('Welcome to the Brain Games!')
     name = welcome_user()
     print('What number is missing in the progression?')
     success = 0
-    for tries in range(0, 3):
-        result = progression() 
-        print(f"Question: {result}")
+    for tries in range(0, 3): 
+        print("Question:", end=' ')
+        result = progression()
         answer = prompt.string("Your answer: ")
         if result == int(answer):
             print("Correct!")
-            success += 1 
+            success += 1
         else:
-            print(f'"{answe}" is wrong answer ;(. Correct answer was "{result}".\nLet\'s try again, {name}!')
+            print(f'"{answer}" is wrong answer ;(. Correct answer was "{result}".\nLet\'s try again, {name}!')
             break 
 
     if success == 3:
