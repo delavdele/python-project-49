f8c420684fe1ef0a639075270d715533f62e0074
Adel Yudina <adelyudina@gmail.com>
2023-04-03 14:23:28 +0300

    updated progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,10 +4,10 @@ general_question = 'What number is missing in the progression?'
 
 
 def get_question_and_answer():
-    start = randint(1, 10)
-    step = randint(1, 10)
+    start = randint(1, 11)
+    step = randint(1, 11)
     stop = start + 10 * step
-    random_ind = randint(1, 10)
+    random_ind = randint(0, 10)
     answer = 0
     index = 0
     question = ''
